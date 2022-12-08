8f88befc6c4059e402e9f3324978737ef335a3d5
Faith <fmschroeder98@gmail.com>
2022-12-08 04:51:06 -0600

    styling changes

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ window.addEventListener("load", function() {
 let listedPlanetsResponse = myFetch()
 listedPlanetsResponse.then(function (result) {
     listedPlanets = result; 
-    console.log(listedPlanets);
 }).then(function () {
-    console.log(listedPlanets);
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
