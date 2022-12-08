fdbc45b4b97d08c4a69f8589875cd83e08ef6b65
Faith <fmschroeder98@gmail.com>
2022-12-08 04:33:25 -0600

    should work now

--- script.js
+++ script.js
@@ -3,9 +3,7 @@
 window.addEventListener("load", function() {  
     let list = document.getElementById("faultyItems");
     list.style.visibility = "hidden";
-
     let listedPlanets;
-
 let listedPlanetsResponse = myFetch()
 listedPlanetsResponse.then(function (result) {
     listedPlanets = result; 
@@ -15,7 +13,6 @@ listedPlanetsResponse.then(function (result) {
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
-
     let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
     let pilotNameInput = document.querySelector("input[name=pilotName]");
