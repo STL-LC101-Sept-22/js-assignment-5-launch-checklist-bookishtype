6a6effb6625e5cec12febdcd6cf23bdf8f669e98
Faith <112731014+bookishtype@users.noreply.github.com>
2022-12-08 12:10:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {  
+  let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+   event.preventDefault();
+       
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let pilot = pilotName.value;
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let copilot = copilotName.value;
+    let fuelLevelVariable = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = Number(fuelLevelVariable.value);
+    let cargoMassVariable = document.querySelector("input[name=cargoMass]");
+    let cargoLevel = Number(cargoMassVariable.value);
     let list = document.getElementById("faultyItems");
     list.style.visibility = "hidden";
-    let listedPlanets;
+ 
+       
+  formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+  
+   });
+    
+let listedPlanets;
 let listedPlanetsResponse = myFetch()
 listedPlanetsResponse.then(function (result) {
     listedPlanets = result; 
+    console.log(listedPlanets);
 }).then(function () {
+    console.log(listedPlanets);
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
-    let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-    let pilotNameInput = document.querySelector("input[name=pilotName]");
-    let pilot = pilotNameInput.value;
-    let copilotNameInput = document.querySelector("input[name=copilotName]");
-    let copilot = copilotNameInput.value;
-    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-    let fuelLevel = Number(fuelLevelInput.value);
-    let cargoMassInput = document.querySelector("input[name=cargoMass]");
-    let cargoLevel = Number(cargoMassInput.value);
     
-    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-    event.preventDefault();
-   });
+    
    
 });
