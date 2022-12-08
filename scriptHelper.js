ae43d66dce38e18f8788c5571f3ee322caed79a6
Faith <fmschroeder98@gmail.com>
2022-12-08 04:16:11 -0600

    is not responding to incorrect data

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,16 +3,17 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   
-                // <h2>Mission Destination</h2>
-                // <ol>
-                //     <li>Name: </li>
-                //     <li>Diameter: </li>
-                //     <li>Star: ${star}</li>
-                //     <li>Distance from Earth: </li>
-                //     <li>Number of Moons: </li>
-                // </ol>
-                // <img src="">
+   let missionTarget = document.getElementById("missionTarget")
+    missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons} </li>
+                </ol>
+                <img src="${imageUrl}">`
    
 }
 
